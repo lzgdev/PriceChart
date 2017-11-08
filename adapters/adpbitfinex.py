c59a9b91bb667ed0dfe6e768f4233302ed6937af
Li ZhiGang <lizg22@163.com>
2017-11-08 13:40:38 +0800

    PriceChart project(depth view): update AdpBitfinexWSS class, for WSS connection to Bitfinex

--- adapters/adpbitfinex.py
+++ adapters/adpbitfinex.py
@@ -16,29 +16,61 @@ class AdpBitfinexWSS(websocket.WebSocketApp):
 		self.on_close = AdpBitfinexWSS.wssEV_OnClose
 		self.auth_api_key = api_key
 		self.auth_api_secret = api_secret
+		self.book_r0_chanid  = None
+		self.book_p0_chanid  = None
+		self.book_p1_chanid  = None
+		self.book_p2_chanid  = None
+		self.book_p3_chanid  = None
 
 	def	wssEV_OnOpen(self):
 		if (self.auth_api_key != None) and (self.auth_api_secret != None):
 			self.wss_auth(self.auth_api_key, self.auth_api_secret)
-#		self.send(json.dumps(payload))
-		self.send('{ "event": "subscribe", "channel": "ticker", "symbol": "tBTCUSD" }')
-#		self.send('{ "event": "subscribe", "channel": "book", "symbol": "tBTCUSD", "prec": "P0", "freq": "F0", "len": 25 }')
-#		self.send('{ "event": "subscribe", "channel": "book", "symbol": "tBTCUSD", "prec": "P1", "freq": "F0", "len": 25 }')
-#		self.send('{ "event": "subscribe", "channel": "book", "symbol": "tBTCUSD", "prec": "P0", "freq": "F0", "len": 100 }')
+#		self.send('{ "event": "subscribe", "channel": "ticker", "symbol": "tBTCUSD" }')
+#		self.send('{ "event": "subscribe", "channel": "book", "symbol": "tBTCUSD", "prec": "R0", "len": 100 }')
+#		self.send('{ "event": "subscribe", "channel": "book", "symbol": "tBTCUSD", "prec": "P0", "freq": "F0", "len":  25 }')
+		self.send('{ "event": "subscribe", "channel": "book", "symbol": "tBTCUSD", "prec": "P0", "freq": "F0", "len": 100 }')
+#		self.send('{ "event": "subscribe", "channel": "book", "symbol": "tBTCUSD", "prec": "P1", "freq": "F0", "len":  25 }')
 #		run_times = 15
-		run_times = 5
+		run_times =  8
 		def run(*args):
 			for i in range(run_times):
 				time.sleep(1)
 				print("wait ...")
-#				self.send('{ "event":"ping" }')
 			time.sleep(1)
 			self.close()
 			print("thread terminating...")
 		_thread.start_new_thread(run, ())
 
 	def	wssEV_OnMessage(self, message):
-		print(message)
+		rsp_obj  = json.loads(message)
+		if type(rsp_obj) is dict and 'event' in rsp_obj:
+			rsp_dict = rsp_obj
+			rsp_list = None
+		elif type(rsp_obj) is list:
+			rsp_dict = None
+			rsp_list = rsp_obj
+		else:
+			rsp_dict = None
+			rsp_list = None
+		if   not rsp_list is None and len(rsp_list) == 2:
+			if not type(rsp_list[1]) is list:
+				print("msg(misc): ", message)
+			elif self.book_p0_chanid == rsp_list[0]:
+				print("msg(book p0): len=", len(rsp_list), "list:", message)
+			elif self.book_p1_chanid == rsp_list[0]:
+				print("msg(book p1): len=", len(rsp_list), "list:", message)
+			elif self.book_r0_chanid == rsp_list[0]:
+				print("msg(book r0): len=", len(rsp_list), "list:", message)
+			else:
+				print("msg(list): ", message)
+		elif not rsp_dict is None and rsp_dict['event'] == 'subscribed':
+			if   rsp_dict['channel'] == 'book' and rsp_dict['prec'] == 'P0':
+				self.book_p0_chanid  = rsp_dict['chanId']
+			elif rsp_dict['channel'] == 'book' and rsp_dict['prec'] == 'P1':
+				self.book_p1_chanid  = rsp_dict['chanId']
+			elif   rsp_dict['channel'] == 'book' and rsp_dict['prec'] == 'R0':
+				self.book_r0_chanid = rsp_dict['chanId']
+			print("msg(dict): ", rsp_dict['event'], message)
 
 	def	wssEV_OnError(self, error):
 		print(error)
