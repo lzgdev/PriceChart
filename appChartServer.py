91bccae2569509565b1496e71bc056027d3c39cc
Li ZhiGang <lizg22@163.com>
2017-12-27 18:13:54 +0800

    PriceChart project: change server site framework to tornado from flask, more implementations.

--- appChartServer.py
+++ appChartServer.py
@@ -1,13 +1,14 @@
 
 import tornado.ioloop
 import tornado.web
+import tornado.gen
 import tornado.websocket
 
 import os.path
 
 gDir_root = os.path.dirname(__file__)
-gDir_ui_static = 'ui/static'
-gDir_demo_html = 'demo/html'
+
+gDir_ui_static = os.path.join(gDir_root, 'ui/static')
 
 class MainHandler(tornado.web.RequestHandler):
 	def initialize(self, code_entry):
@@ -17,32 +18,36 @@ class MainHandler(tornado.web.RequestHandler):
 		path_pref = None
 		path_file = os.path.basename(self.request.path)
 		if   self.code_entry == 'home':
-			return self.render(gDir_ui_static + '/html/index.html')
+			return self.render('index.html')
 		elif self.code_entry == 'views':
-			path_pref = 'ui/html/views'
+			path_pref = 'views'
+		#return self.render(os.path.join(path_pref, path_file), dev_js29_ver=1122)
 		return self.render(os.path.join(path_pref, path_file),
 								dev_js29_ver=1122)
+
+
+class HomeHandler(tornado.web.StaticFileHandler):
+	def validate_absolute_path(self, root, absolute_path):
+		if self.request.uri == '/':
+			absolute_path = os.path.join(absolute_path, "html")
+		print("HomeHandler: root=" + root + ", absolute_path=" + absolute_path + ", request=" + str(self.request))
+		return super(HomeHandler, self).validate_absolute_path(root, absolute_path)
 
 class DemoHandler(tornado.web.RequestHandler):
 	def get(self):
 		path_file = os.path.basename(self.request.path)
-		return self.render(os.path.join(gDir_demo_html, path_file))
+		return self.render(path_file)
 
-class MiscHandler(tornado.web.RequestHandler):
-	def initialize(self, mime_type):
-		self.mime_type = mime_type
+	def get_template_path(self):
+		return os.path.join(gDir_root, 'demo/html')
 
-	def get(self):
-		path_pref = None
+class MiscHandler(tornado.web.StaticFileHandler):
+	def validate_absolute_path(self, root, absolute_path):
 		path_file = os.path.basename(self.request.path)
-		if   self.mime_type == 'text/css':
-			path_pref = gDir_ui_static + '/css/'
-		elif self.mime_type == 'text/javascript':
-			if   os.path.isfile(os.path.join(gDir_root, 'code/js', path_file)):
-				path_pref = 'code/js'
-			else:
-				path_pref = os.path.join(gDir_ui_static, 'js')
-		return self.render(os.path.join(path_pref, path_file))
+		#print("MiscHandler: root=" + root + ", absolute_path=" + absolute_path + ", request=" + str(self.request))
+		if os.path.isfile(os.path.join(gDir_root, 'code/js', path_file)):
+			return os.path.join(gDir_root, 'code/js', path_file)
+		return super(MiscHandler, self).validate_absolute_path(root, absolute_path)
 
 class SocketHandler(tornado.websocket.WebSocketHandler):
 	def check_origin(self, origin):
@@ -58,16 +63,23 @@ class SocketHandler(tornado.websocket.WebSocketHandler):
 
 class AppChartServer(tornado.web.Application):
 	def __init__(self):
+		settings = dict(
+			debug=True,
+			template_path=os.path.join(gDir_root, 'ui/templates'),
+			static_path=os.path.join(gDir_root, 'ui/static'),
+		)
 		handlers = [
-			(r'/',               MainHandler, { 'code_entry': 'home', }),
 			(r'/views/.*\.html', MainHandler, { 'code_entry': 'views', }),
 			(r'/demo/.*\.html',  DemoHandler),
 			(r'/ws', SocketHandler),
-			(r'/css/.*\.css', MiscHandler, { 'mime_type': 'text/css', }),
-			(r'/js/.*\.js',   MiscHandler, { 'mime_type': 'text/javascript', }),
-			(r'/(favicon.ico)', tornado.web.StaticFileHandler, { 'path': gDir_ui_static + '/icons', }),
+			(r'/(css/.+\.css)', MiscHandler, { 'path': settings['static_path'] }),
+			(r'/(js/.+\.js)',   MiscHandler, { 'path': settings['static_path'] }),
+			(r'/(favicon.ico)', MiscHandler, { 'path': settings['static_path'] }),
+			#(r'/(favicon.ico)', tornado.web.StaticFileHandler, dict(path=settings['static_path'])),
+			#(r'/(.*)',           HomeHandler, { 'path': 'ui/static', 'default_filename': 'index.html', }),
+			(r'/()',           HomeHandler, { 'path': 'ui/static', 'default_filename': 'index.html', }),
 		]
-		super(AppChartServer, self).__init__(handlers)
+		super(AppChartServer, self).__init__(handlers, **settings)
 
 def main():
 	app = AppChartServer()
